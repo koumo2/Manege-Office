778f3df3361db88e836689bf060f30e8818a0c42
甲元和馬 <koumotokazuma@koumotokazumanoMacBook-Air.local>
2020-11-29 15:04:00 +0900

    11/29 scheduleホーム画面実装

--- resources/views/schedule/home.blade.php
+++ resources/views/schedule/home.blade.php
@@ -1,7 +1,126 @@
 @extends('layouts.menuber')
 
-@section('title', "回覧板")
+@section('title', "スケジュール")
 @section('content')
+<?php
+// タイムゾーンを設定
+date_default_timezone_set('Asia/Tokyo');
 
+// 前月・次月リンクが押された場合は、GETパラメーターから年月を取得
+if (isset($_GET['ym'])) {
+    $ym = $_GET['ym'];
+} else {
+    // 今月の年月を表示
+    $ym = date('Y-m');
+}
+
+// タイムスタンプを作成し、フォーマットをチェックする
+$timestamp = strtotime($ym . '-01');
+if ($timestamp === false) {
+    $ym = date('Y-m');
+    $timestamp = strtotime($ym . '-01');
+}
+
+// 今日の日付 フォーマット
+$today = date('Y-m-j');
+
+// カレンダーのタイトルを作成
+$html_title = date('Y年n月', $timestamp);
+
+// 前月・次月の年月を取得
+$prev = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)-1, 1, date('Y', $timestamp)));
+$next = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)+1, 1, date('Y', $timestamp)));
+
+// 該当月の日数を取得
+$day_count = date('t', $timestamp);
+
+// １日が何曜日か
+$youbi = date('w', mktime(0, 0, 0, date('m', $timestamp), 1, date('Y', $timestamp)));
+
+// カレンダー作成の準備
+$weeks = [];
+$week = '';
+
+// 第１週目：空のセルを追加
+$week .= str_repeat('<td></td>', $youbi);
+
+for ( $day = 1; $day <= $day_count; $day++, $youbi++) {
+    $date = $ym . '-' . $day;
+    if ($today == $date) {
+        // 今日の日付の場合は、class="today"をつける
+        $week .= '<td class="today">' . $day;
+    } else {
+        $week .= '<td>' . $day;
+    }
+    $week .= '</td>';
+    // 週終わり、または、月終わりの場合
+    if ($youbi % 7 == 6 || $day == $day_count) {
+        if ($day == $day_count) {
+            // 月の最終日の場合、空セルを追加
+            $week .= str_repeat('<td></td>', 6 - ($youbi % 7));
+        }
+        // weeks配列にtrと$weekを追加する
+        $weeks[] = '<tr>' . $week . '</tr>';
+        // weekをリセット
+        $week = '';
+	}
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <style>
+    .container {
+        font-family: 'Noto Sans', sans-serif;
+            margin-top: 0px;
+        }
+        h3 {
+            margin-bottom: 30px;
+        }
+        th {
+            height: 30px;
+            text-align: center;
+        }
+        td {
+            height: 100px;
+        }
+        .today {
+            background: orange;
+        }
+        th:nth-of-type(1), td:nth-of-type(1) {
+            color: red;
+        }
+        th:nth-of-type(7), td:nth-of-type(7) {
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+<h2>・スケジュール</h2>
+<div class = home_main>
+    <div class="container">
+    <h3><a href="?ym=<?php echo $prev; ?>">&lt;</a> <?php echo $html_title; ?> <a href="?ym=<?php echo $next; ?>">&gt;</a></h3>
+        <table class="table table-bordered">
+            <tr>
+                <th>日</th>
+                <th>月</th>
+                <th>火</th>
+                <th>水</th>
+                <th>木</th>
+                <th>金</th>
+                <th>土</th>
+            </tr>
+            <?php
+                foreach ($weeks as $week) {
+                    echo $week;
+                }
+            ?>
+        </table>
+    </div>
+    </div>
+</body>
+</html>
 
 @endsection
